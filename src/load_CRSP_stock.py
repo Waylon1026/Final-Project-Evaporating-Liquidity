908de9bf4099d0e86810c830ef4fd5408a91c6ae
junhan <jasonfu@uchicago.edu>
2024-02-25 15:39:47 -0600

    Update load_CRSP_stock

--- src/load_CRSP_stock.py
+++ src/load_CRSP_stock.py
@@ -100,13 +100,13 @@ def load_CRSP_index_files(data_dir=DATA_DIR):
     return df
 
 def demo():
-    # crsp = load_CRSP_daily_file(data_dir=DATA_DIR)
+    crsp = load_CRSP_daily_file(data_dir=DATA_DIR)
     df_msix = load_CRSP_index_files(data_dir=DATA_DIR)
 
 
 if __name__ == "__main__":
-    # crsp = pull_CRSP_daily_file(wrds_username=WRDS_USERNAME)
-    # crsp.to_parquet(DATA_DIR / "pulled" / "CRSP_stock.parquet")
+    crsp = pull_CRSP_daily_file(wrds_username=WRDS_USERNAME)
+    crsp.to_parquet(DATA_DIR / "pulled" / "CRSP_stock.parquet")
 
     df_msix = pull_CRSP_index_files(start_date=START_DATE, end_date=END_DATE)
     path = Path(DATA_DIR) / "pulled" / f"CRSP_DSIX.parquet"
